2ce8f2638f3c138b9e2cd49783cf59d870a89936
YoshioSM <yoshiosotomontes@hotmail.com>
2025-09-07 13:39:12 -0600

    update

--- site/src/Pages/Home/index.jsx
+++ site/src/Pages/Home/index.jsx
@@ -89,7 +89,7 @@ export default function Home() {
                 <div className="absolute inset-0 bg-gradient-to-br from-blue-400/20 to-cyan-400/20 rounded-3xl transform rotate-6"></div>
                 <Stack
                   randomRotation={true}
-                  sensitivity={80}
+                  sensitivity={140}
                   sendToBackOnClick={true}
                   cardDimensions={{ width: 290, height: 290 }}
                 />
